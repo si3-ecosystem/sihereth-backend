4751b00a3f91954c550f6d4d85e8145001b805b9
zas512 <zainalis.914@gmail.com>
2025-06-17 23:54:52 +0500

    Revert "invite link fix"
    This reverts commit 6c784929a9ab000e91dbedcdf3ded4f225a3035d.

--- models/User.model.js
+++ models/User.model.js
@@ -12,7 +12,7 @@ const UserSchema = new mongoose.Schema(
 );
 
 UserSchema.pre("save", async function (next) {
-  if (!this.isModified("password") || !this.password) return next();
+  if (!this.isModified("password")) return next();
   this.password = await bcrypt.hash(this.password, 10);
   next();
 });
